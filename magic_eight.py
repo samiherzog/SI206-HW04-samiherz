fa906a285b5d821e4214669520820736ecf1dd11
Samantha Herzog <samiherzog@0587360996.wireless.umich.net>
2018-01-30 15:17:27 -0500

    second part

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,8 @@
 
 def ask_question():
     x= input('What is your question?')
-    return 'Your question is: ' + x
+    while '?' not in x:
+        print('I am sorry, I can only answer questions')
+        x= input('What is your question?')
+    while x =! 'Quit':
+        x= input('What is your quesiton?')
