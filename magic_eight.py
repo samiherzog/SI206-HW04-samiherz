593a7ca7011f152bba440084401e04af41072405
William Godley <wmgodley@umich.edu>
2018-01-30 15:15:29 -0500

    Added my code

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,4 @@
-
+from random import randrange
 def ask_question():
     x= input('What is your question?')
     return 'Your question is: ' + x
@@ -7,4 +7,14 @@ def ask_question():
 
 
 
+
 # This is me adding branch add_questionsC
+
+def get_answerC():
+
+    answers = ["Yes definitely" , "You may rely on it", "Yes", "Signs point to yes",
+        "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+        "My sources say no", "Outlook not so good", "Very doubtful"]
+    index = randrange(len(answers))
+    answer = answers[index]
+    return answer
